fb5895f69b6352a34c5365d9246799f189c3216a
Hendrik Liebau <mail@hendrik-liebau.de>
2023-10-10 18:47:50 +0200

    Add a build script

---
+++ build.js
@@ -0,0 +1,4 @@
+import * as esbuild from 'esbuild';
+import {options} from './esbuild-options.js';
+
+await esbuild.build(options);
--- start.js
+++ start.js
@@ -1,13 +1,7 @@
 import * as esbuild from 'esbuild';
+import {options} from './esbuild-options.js';
 
-let ctx = await esbuild.context({
-  entryPoints: ['src/integrator.tsx', 'src/feature-app.tsx'],
-  outdir: 'public/js',
-  bundle: true,
-  format: 'esm',
-  external: ['react', 'react-dom'],
-  sourcemap: true,
-});
+let ctx = await esbuild.context(options);
 
 await ctx.watch();
 
